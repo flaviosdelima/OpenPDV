c4d8255a3b8e98b3f2a584651da5dfaeb048858d
Flavio <euprogramo@gmail.com>
2018-12-26 23:20:40 -0200

    Alteração para ElectronJs

---
+++ index.js
@@ -0,0 +1,9 @@
+const electron = require('electron');
+
+const {app, BrowserWindow} = electron;
+
+app.on('ready',()=>{
+    const janelaPrincipal = new BrowserWindow({});
+    janelaPrincipal.loadURL(`file://${__dirname}/index.html`);
+    //console.log('App is now!!');
+});
